31d16072e1387d6d992ce4501ca5de8eba95d5e5
Ethan Donovan <49061190+SoberBluee@users.noreply.github.com>
2023-04-22 15:40:30 +0100

    comment sections speech bubbles

--- src/app/shared/components/comments-section/comment-section.component.ts
+++ src/app/shared/components/comments-section/comment-section.component.ts
@@ -10,5 +10,18 @@ export class CommentSectionComponent implements OnInit {
 
     constructor() {}
 
-    ngOnInit() {}
+    ngOnInit() {
+        this.comments = [
+            {
+                id: 1,
+                comment:
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nunc diam, volutpat non gravida non, dignissim non mauris. Nulla sed mollis orci. In sit amet lacus tincidunt, tristique eros vitae, consequat risus. Sed porta nisl ante. Ut volutpat viverra enim nec efficitur. Nullam at tristique ex. Aliquam tempor, lacus a tincidunt convallis, eros magna mollis risus, et venenatis massa tortor at justo. Praesent rutrum mauris nec diam convallis pellentesque. Aliquam gravida, enim a dignissim blandit, ante enim euismod diam, at malesuada nunc ipsum ut dui. Nam accumsan faucibus facilisis. ',
+            },
+            {
+                id: 1,
+                comment:
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nunc diam, volutpat non gravida non, dignissim non mauris. Nulla sed mollis orci. In sit amet lacus tincidunt, tristique eros vitae, consequat risus. Sed porta nisl ante. Ut volutpat viverra enim nec efficitur. Nullam at tristique ex. Aliquam tempor, lacus a tincidun',
+            },
+        ];
+    }
 }
